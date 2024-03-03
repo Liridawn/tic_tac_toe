50489e19f98793096c08371cf4333d02c86c56a2
Liridawn <liridon.bislimi@hotmail.com>
2024-03-03 22:44:07 +0100

    First commit on game

---
+++ tic_tac_toe.py
@@ -0,0 +1,4 @@
+# Build a tic-tac-toe game or "engine" that allow human players to play against each other
+# Plug AI into our game-engine
+# Repeatedly play AIs against each other to see which of them is the best
+
